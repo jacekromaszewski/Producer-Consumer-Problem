628f8d9f3920267f11688e96181f1b84568a1ca5
Jacek Romasevski <jacek.romasevski@student.wat.edu.pl>
2019-07-09 18:42:25 +0300

    Add project files.

---
+++ Producer-Consumer Problem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Producer_Consumer_Problem
+{
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Hom many producer will produce each time: ");
+                int ProducerValue = Int32.Parse(Console.ReadLine());
+                Console.Write("Amount of consumers: ");
+                int ConsumerCount = Int32.Parse(Console.ReadLine());
+
+                var Store = new Store(ProducerValue);
+
+                Thread Producer = new Thread(new ThreadStart(Store.ProducerThings));
+                Producer.Start();
+                for (int i = 0; i < ConsumerCount; i++)
+                {
+                    Thread Consumer = new Thread(new ThreadStart(Store.ConsumerThings));
+                    Consumer.Start();
+                }
+
+                while (true) { }
+            }
+        }
+    }
---
+++ Producer-Consumer Problem/Store.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace Producer_Consumer_Problem
+{
+    class Store
+    {
+        readonly Random Random = new Random();
+        private readonly object _Monitor = new object();
+        private int _Capacity = 1000;
+        private int _Current = 0;
+        private readonly int _ProducerValue;
+        public Store(int ProducerValue)
+        {
+            _ProducerValue = ProducerValue;
+        }
+        public Store(int ProducerValue,int Capacity, int Current)
+        {
+            _ProducerValue = ProducerValue;
+            _Capacity = Capacity;
+            _Current = Current;
+
+        }
+        public void StoreUsage(int i)
+        {
+            if (i > 0)
+                if ((i + _Current) > _Capacity)
+                    _Current = _Capacity;
+                else _Current += i;
+            if (i < 0)
+                if ((i + _Current) < 0)
+                    _Current = 0;
+                else _Current += i;
+        }
+        public void ProducerThings()
+        {
+            while (true)
+                lock (_Monitor)
+                {
+                    while (_Current == _Capacity)
+                        Monitor.Wait(_Monitor);
+                    StoreUsage(_ProducerValue);
+                    Console.WriteLine("P store: {0}", _Current);
+                    Thread.Sleep(500);
+                    Monitor.PulseAll(_Monitor);
+                }
+        }
+        public void ConsumerThings()
+        {
+            while (true)
+                lock (_Monitor)
+                {
+                    while (_Current == 0)
+                        Monitor.Wait(_Monitor);
+                    StoreUsage(-Random.Next(_ProducerValue));
+                    Console.WriteLine("C store: {0}", _Current);
+                    Thread.Sleep(500);
+                    Monitor.PulseAll(_Monitor);
+                }
+        }
+    }
+}
